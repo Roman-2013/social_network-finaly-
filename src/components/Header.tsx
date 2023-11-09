21296601ef7401aec56d79357968f13c30d329a1
Roman-2013 <roma.pushkarev.1995@mail.ru>
2023-11-09 12:41:57 +0300

    #12 (Header,NavBar,Profile component)

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className={'header'}>
+            <img src="https://pm1.aminoapps.com/7741/5d2386f9f374a5b82ca80394c712b4f027f0d8der1-720-699v2_00.jpg"/>
+        </header>
+    );
+};
+
---
+++ src/components/Profile.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className={'content'}>
+            <div>
+                <img
+                    src="https://sun9-65.userapi.com/impg/c857736/v857736321/1034e2/DywLDwd6CeA.jpg?size=807x325&quality=96&sign=2d19ba00a905cdbe81732042935997de&type=album"
+                    alt=""/>
+            </div>
+            <div>ava+decription</div>
+            <div>My post</div>
+            <div>New post</div>
+            <div>Post 1</div>
+            <div>Post 2</div>
+            <div>Post 3</div>
+            Content
+        </div>
+    );
+};
+
