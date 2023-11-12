9b7ba23f2e0f51091d36779fb04760c15431cb75
Roman-2013 <roma.pushkarev.1995@mail.ru>
2023-11-12 23:21:16 +0300

    #21 (page Dialogs)

--- src/components/Dialogs/Message.tsx
+++ src/components/Dialogs/Message.tsx
@@ -3,8 +3,17 @@ import s from './Message.module.css'
 
 export const Message = () => {
     return (
-        <div>
-            Messages
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>Dima</div>
+                <div className={s.dialog}>Roma</div>
+                <div className={s.dialog}>Ilya</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>  You are coll man</div>
+            </div>
         </div>
     );
 };
