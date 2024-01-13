9e9d6a9fd7166a7c8a14deef79a50bd20de8da24
Roman-2013 <roma.pushkarev.1995@mail.ru>
2024-01-14 00:01:01 +0300

    #74 (componentDidUpdate)

--- src/components/Profile/ProfileStatus/ProfileStatus.tsx
+++ src/components/Profile/ProfileStatus/ProfileStatus.tsx
@@ -29,6 +29,13 @@ export class ProfileStatus extends React.Component<ProfileStatusType> {
         this.setState({status: e.currentTarget.value})
     }
 
+
+     componentDidUpdate(prevProps:any,prevState:any) {
+    if(prevProps.status!==this.props.status){
+        this.setState({status:this.props.status})
+    }
+     }
+
     render() {
         return (
             this.state.editMode
